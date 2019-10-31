21b55c866d54d525be07db8b426470b4e90f8283
svcoverman <vsaul0711@gmail.com>
2019-10-30 22:00:42 -0500

    Adding root files

---
+++ db/seeds.sql
@@ -0,0 +1,16 @@
+USE burger_db;
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("BigMac", FALSE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("PattyMelt", FALSE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("AmericanCheeseBurger", FALSE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Whooper", FALSE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("QuarterPounder", FALSE);
