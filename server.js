acf51a0b989b140fc556c3523fc202a8ce1415f8
svcoverman <vsaul0711@gmail.com>
2019-11-02 23:15:38 -0500

    heroku upload

--- server.js
+++ server.js
@@ -3,15 +3,10 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-// Set the port of our application
-// process.env.PORT lets the port be set by Heroku
 const PORT = process.env.PORT || 8080;
 
-// lets client side files use relative file paths
-// specifically the public folder
 app.use(express.static("public"));
 
-// Parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -20,14 +15,10 @@ const exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// connects api routes to the server
 const routes = require("./controllers/burgers_controller");
 
-// allows the app (express) to use the var routes
 app.use(routes);
 
-// Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
-    // Log (server-side) when our server has started
     console.log("Server listening on: http://localhost:" + PORT);
   });
